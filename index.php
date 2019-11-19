6ab7ff7f5e436b99815e6ce58642f90c1d6775b2
ragnarbarak <57958497+ragnarbarak@users.noreply.github.com>
2019-11-19 15:44:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php                    
-  echo "<h1>Openshift Workshop v1.0</h1> ";                    
+  echo "<h1>Openshift Workshop v2.0 - atualizado com webhook</h1> ";                    
   $host= gethostname();
   $ip = gethostbyname($host);                    
   echo $ip;
