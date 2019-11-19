520be81493f44e3ad8aabb8004068d1f297539ce
ragnarbarak <57958497+ragnarbarak@users.noreply.github.com>
2019-11-19 15:58:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,19 @@
-<?php                    
-  echo "<h1>Openshift Workshop v3.1 - agora com cidades</h1> ";                    
-  echo $_SERVER['SERVER_ADDR'];
-  $host= gethostname();
-  $ip = gethostbyname($host);                    
-  echo $ip;
-  echo "<br><hr>";
-  echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
-  $conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
-  if ($conn->connect_error) {
+<?php
+echo "<h1>Openshift Workshop v2.0</h1> ";
+echo $_SERVER['SERVER_ADDR'];
+echo "<br><hr>";
+echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
+$conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-  }
-  $result = $conn->query("SELECT nome FROM cidade");
-  if ($result->num_rows > 0) {
+}
+$result = $conn->query("SELECT nome FROM cidade");
+if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<h3>" . $row["nome"] . "</h3>";
     }
-  } else {
+} else {
     echo "0 results";
-  }
-  $conn->close();
-?>  
-
+}
+$conn->close();
+?>
